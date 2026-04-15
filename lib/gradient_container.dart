c7a80075c5c0592b8402c9d1940bad9a35b9adba
vika <vikatekutova7@gmail.com>
2026-04-15 15:17:22 +0300

    Refactor dice logic into StatefulWidget DiceRoller

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,21 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_lab3_app/dice_roller.dart';
 
 const startAlignment = Alignment.topCenter;
 const endAlignment = Alignment.bottomCenter;
 
 class GradientContainer extends StatelessWidget {
-  GradientContainer(this.color1, this.color2, this.color3, {super.key});
+  const GradientContainer(this.color1, this.color2, this.color3, {super.key});
 
   final Color color1;
   final Color color2;
   final Color color3;
-
-  var activeDiceImage = 'assets/images/dice-1.png';
-
-  void rollDice() {
-    activeDiceImage = 'assets/images/dice-4.png';
-    print('Изменили картинку');
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -31,27 +25,8 @@ class GradientContainer extends StatelessWidget {
           end: endAlignment,
         ),
       ),
-      child: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Image.asset(
-              activeDiceImage,
-              width: 300,
-            ),
-            const SizedBox(height: 20),
-            TextButton(
-              onPressed: rollDice,
-              style: TextButton.styleFrom(
-                foregroundColor: Colors.lime,
-                textStyle: const TextStyle(
-                  fontSize: 30,
-                ),
-              ),
-              child: const Text('Roll Dice'),
-            ),
-          ],
-        ),
+      child: const Center(
+        child: DiceRoller(),
       ),
     );
   }
